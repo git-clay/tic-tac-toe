7b7def2c5344acd5c25b47c64b58aae8b5426693
Clayton Smith <claybin@claytons-MacBook-Pro.local>
2016-11-09 16:45:16 -0700

    html as table, very little js

---
+++ main.js
@@ -0,0 +1,40 @@
+//locate elements to use (document.getElementById, document.getElementByClassName,etc)
+$(document).ready(function(){
+
+var $findCell = $('id').click(function(){
+	console.log($findCell);
+});
+
+/*
+var $findColumn = function(){
+	document.getElementsByClassName('column');
+	console.log(findColumn);
+} ;
+	
+*/
+
+
+
+//onclick function to select and imput x or o for current player
+var selectTile =function(onclick){
+
+};
+
+
+//function to determine current player
+
+//make text to display current player
+
+//function to determine winner
+//alert
+
+
+
+
+
+/*---------optional---------*/
+//Display message at start of game
+//alert if 3 wins in a row
+//scoreboard
+//css animations (link on github readme for help)
+});
