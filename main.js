af62f991a42de4f623cf13f63224bab03723470d
Clayton Smith <claybin@claytons-MacBook-Pro.local>
2016-11-10 15:59:31 -0700

    oop started in js

--- main.js
+++ main.js
@@ -2,11 +2,27 @@
 //√cell number >into> $currentId >into> div
 //√onclick function to select and imput x or o for current player
 $(document).ready(function(){
-var turn = 0;
 var exArray=[];
 var ohArray=[];	
 
+/*constructor:creates on/off tiles >> x/o tile
+	//id of tile //state of tile bool //content of tile(x,o)*/
+function Tile () {		
+					
+
+
+}
+var turn = {		//current turn //attributes	//
+
+};
+
+//object 
+
+
+
+});
 //√odd turns will be oh. even will be ex.
+/*
 console.log('outside');
 $('h3').html("It is X's turn");
 
@@ -34,19 +50,25 @@ $('.cell').click(function(){				//searches for the .cell class and waits for cli
 
 
 });
-
+*/
 //√function to determine current player
-//make text to display current player
-//append body and create h3 for current player
+//√make text to display current player
+//√append body and create h3 for current player
 
 //function to determine winner
 //alert winner!
 	/* logic plan
 	ex array
 	oh array
-		if (1,5,9||3,5,7){Diagonal win!}
-		else if(1,4,7||2,5,8||3,6,9){vertical win!}
-		else if(123,456,789){horizontal win!}
+		if [1,5,9] || [3,5,7]{Diagonal win!}
+		else if() [1,4,7] || [2,5,8] || [3,6,9] ){vertical win!}
+		else if([1,2,3] || [4,5,6] || [7,8,9] ){horizontal win!}
+  /////$jQuery.inArray(10,["8","9","10",10]); //returns -1 if !found ||index if found
+  /////$jQuery.inArray("John", arr); (found at index 3)||   /////$jQuery.inArray(4, arr); (!found, returns -1) ||   
+  /////$jQuery.inArray("Pete", arr,2); (in array but not at or after index 2===return -1)
+  //////for loop (sof-> screenshot on phone)
+
+
 	*/
 //reset
 
