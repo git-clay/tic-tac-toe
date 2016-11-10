9a96ed78235c7a607b9bd55aaa46b3938f6df38b
Clayton Smith <claybin@claytons-MacBook-Pro.local>
2016-11-09 20:46:18 -0700

    divs grid set - progress on click appender

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 //locate elements to use (document.getElementById, document.getElementByClassName,etc)
 $(document).ready(function(){
-
-var $findCell = $('id').click(function(){
-	console.log($findCell);
+var turn = 0;
+//odd turns will be oh. even will be ex.
+//if (turn%2 ===1){		//if the turn is odd (oh)
+var $currentId = $('.cell').click(function(){	//looks for cell and waits for a click
+	var current = this.id;
+	$('current').append('div').append('<h2>').append('O');
+	console.log(this.id);		//this referest to the current cell so this.id give the .id number
+	console.log(this.class);
+	turn++;
+	console.log(turn);
 });
+//function create new class .ex or .oh onto div
 
 /*
-var $findColumn = function(){
-	document.getElementsByClassName('column');
-	console.log(findColumn);
-} ;
-	
+var $newEx = $(function(){
+	$('#ex')
+});
 */
-
-
+//cell number >into> $currentId >into> div
 
 //onclick function to select and imput x or o for current player
 var selectTile =function(onclick){
-
+	//if player 
 };
 
 
