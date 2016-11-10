6debd9282b86a6afdc9eb80257f4fd454bc3538b
Clayton Smith <claybin@claytons-MacBook-Pro.local>
2016-11-09 23:00:45 -0700

    arrays set up to store x&os on board to check for winner

--- main.js
+++ main.js
@@ -1,45 +1,49 @@
 //locate elements to use (document.getElementById, document.getElementByClassName,etc)
+//√cell number >into> $currentId >into> div
+//√onclick function to select and imput x or o for current player
 $(document).ready(function(){
-var turn = 1;
-//odd turns will be oh. even will be ex.
+var turn = 0;
+var exArray=[];
+var ohArray=[];	
 
-$('.cell').click(function(){			//searches for the .cell class and waits for click
-	if (turn%2 ===1){		//if the turn is odd (oh)
+//√odd turns will be oh. even will be ex.
+console.log('outside');
+$('.cell').click(function(){				//searches for the .cell class and waits for click
+	if (turn%2 ===1){						//if the turn is odd (oh)
 		$(this).attr('class','oh cell');	//this refers to the div that was clicked. applies the classes .oh and .cell
-		$(this).append('<h2>O</h2>');
-		turn ++;
-		console.log(turn);
+		$(this).append('<h2>O</h2>');		//adds an h2 with an o in it
+		exArray.push($(this).attr('id'));
+		turn ++;							//advances the turn
 	}
-	else if(turn%2===0){		//turn is even (ex)
-		$(this).attr('class','ex cell');
+	else if(turn%2===0){					//turn is even (ex)
+		$(this).attr('class','ex cell');	//adds an h2 in the selected div with X in it
 		$(this).append('<h2>X</h2>');
+		ohArray.push($(this).attr('id'));
 		turn ++;
-		console.log(turn);
 	}
+
+	console.log(exArray);
+	console.log(ohArray);
+
+	});
+
+
 });
 
-//function create new class .ex or .oh onto div
-
-/*
-var $newEx = $(function(){
-	$('#ex')
-});
-*/
-//cell number >into> $currentId >into> div
-
-//onclick function to select and imput x or o for current player
-
-// $('div').click(function() {
-});
-
-//function to determine current player
-
+//√function to determine current player
 //make text to display current player
+//append body and create h3 for current player
 
 //function to determine winner
-//alert
-
-
+//alert winner!
+	/* logic plan
+	ex array
+	oh array
+		if (1,5,9||3,5,7){Diagonal win!}
+		else if(1,4,7||2,5,8||3,6,9){vertical win!}
+		else if(123,456,789){horizontal win!}
+	*/
+//reset
 
 
 
