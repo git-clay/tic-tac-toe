843d784b720180bfd3e304a224dd25dc64ada4bc
Clayton Smith <claybin@claytons-MacBook-Pro.local>
2016-11-12 12:42:43 -0700

    reset button works

--- main.js
+++ main.js
@@ -5,31 +5,53 @@ $(document).ready(function(){
 var turn = 0;
 var exArray=[];
 var ohArray=[];	
-
+var win ={
+	a:[1,5,9],	//win diag
+	b:[3,5,7],	//win diag
+	c:[1,4,7],	//win vert
+	d:[2,5,8],	//win vert
+	e:[3,6,9],	//win vert
+	f:[1,2,3],	//win side
+	g:[4,5,6],	//win side
+	h:[7,8,9]	//win side
+};
+var look = function (winCheck,playerArr){
+	return playerArr.some(function(v){
+		return winCheck.indexOf(v) >=0;
+	});
+};
 //√odd turns will be oh. even will be ex.
 console.log('outside');
 $('h3').html("It is X's turn");
 
 $('.cell').click(function(){				//searches for the .cell class and waits for click
 	if (turn%2 ===1){						//if the turn is odd (oh)
-
 		$('h3').html("It is X's turn");
 		$(this).attr('class','oh cell');	//this refers to the div that was clicked. applies the classes .oh and .cell
 		$(this).append('<h2>O</h2>');		//adds an h2 with an o in it
-		exArray.push($(this).attr('id'));
+		ohArray.push($(this).attr('id'));	//adds this id to player's array to track for a win
 		turn ++;							//advances the turn
 	}
 	else if(turn%2===0){					//turn is even (ex)
 		$('h3').html("It is O's turn");
 		$(this).attr('class','ex cell');	//adds an h2 in the selected div with X in it
 		$(this).append('<h2>X</h2>');
-		ohArray.push($(this).attr('id'));
+		exArray.push($(this).attr('id'));
 		turn ++;
 	}
-
+	console.log(look(win.a,exArray));
 	console.log(exArray);
 	console.log(ohArray);
-
+/*		if (win.a||win.b){
+			alert('Diagonal win!');
+		}
+		else if(win.c||win.d||win.e){
+			alert('vertical win!');
+		}
+		else if(win.f||win.g||win.h){
+			alert('horizontal win!');
+		}
+*/		
 	});
 
 
@@ -41,15 +63,14 @@ $('.cell').click(function(){				//searches for the .cell class and waits for cli
 
 //function to determine winner
 //alert winner!
-	/* logic plan
-	ex array
-	oh array
-		if (1,5,9||3,5,7){Diagonal win!}
-		else if(1,4,7||2,5,8||3,6,9){vertical win!}
-		else if(123,456,789){horizontal win!}
-	*/
-//reset
+//	 logic plan
 
+		
+	
+/*Reset button*/
+function reset(){
+	location.reload();
+}
 
 
 /*---------optional---------*/
